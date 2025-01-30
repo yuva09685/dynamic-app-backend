8f63ab1c1a089caa5d79470832e7b79d3b7a6c28
yuva09685 <160525804+yuva09685@users.noreply.github.com>
2025-01-30 17:18:27 +0530

    created production environment credentials
    created production environment credentials

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,9 @@ const { User } = require("../models");
 
 // Generate Gravatar URL
 const getGravatarUrl = (email) => {
-  const hash = crypto.createHash("md5").update(email.trim().toLowerCase()).digest("hex");
+  console.log(email);
+  
+  const hash = crypto.createHash("md5").update(email).digest("hex");
   return `https://www.gravatar.com/avatar/${hash}?s=200&d=identicon`;
 };
 
@@ -15,6 +17,7 @@ const createProfile = async (req, res) => {
     const user = await User.create({ name, email, bio, location, avatar });
     res.json(user);
   } catch (error) {
+    console.log(error)
     res.status(500).json({ error: "Error saving profile" });
   }
 };
